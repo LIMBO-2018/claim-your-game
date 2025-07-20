90686c6349880ef6aea20eb8ab980a765fb78a8c
LIMBO <kaungsettwin999@gmail.com>
2025-07-20 18:36:39 +0630

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,23 +1,17 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+// firebase-config.js (Compat style)
+const firebaseConfig = {
+  apiKey: "AIzaSyBLHRnFtECe0vY3yiES4ZBzRrHYivkNsHc",
+  authDomain: "claimyourgames-122c6.firebaseapp.com",
+  projectId: "claimyourgames-122c6",
+  storageBucket: "claimyourgames-122c6.appspot.com",
+  messagingSenderId: "987221975249",
+  appId: "1:987221975249:web:b8c70b8d65fdba9ed065f0",
+  measurementId: "G-7TJH50ZNL0"
+};
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyBLHRnFtECe0vY3yiES4ZBzRrHYivkNsHc",
-    authDomain: "claimyourgames-122c6.firebaseapp.com",
-    projectId: "claimyourgames-122c6",
-    storageBucket: "claimyourgames-122c6.firebasestorage.app",
-    messagingSenderId: "987221975249",
-    appId: "1:987221975249:web:b8c70b8d65fdba9ed065f0",
-    measurementId: "G-7TJH50ZNL0"
-  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+// Firebase services references
+const auth = firebase.auth();
+const db = firebase.firestore();
